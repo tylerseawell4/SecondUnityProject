80c4e095ea29847f8d30880631417e6a6e4f6224
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-08-31 22:48:00 -0400

    Added Bounce to player

---
+++ SecondUnityProject/Assets/Scripts/TotalBounceMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalBounceMovement : MonoBehaviour {
+    public Rigidbody2D thePlayerRigidBody;
+    public int numberOfBounces;
+    public float playerSpeed;
+    public float moveSpeed;
+    public float nextBounceIncrease;
+    
+    private int _bounceHits = 0;
+    private float _highestYCoordinate;
+    private float _currentYCoordinate;
+    
+
+    // Use this for initialization
+    void Start () {
+        numberOfBounces = 3;
+        playerSpeed = 2;
+        _highestYCoordinate = thePlayerRigidBody.position.y;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {        
+        _currentYCoordinate = thePlayerRigidBody.position.y;
+
+        if (_currentYCoordinate > _highestYCoordinate)
+        {
+            _highestYCoordinate = thePlayerRigidBody.position.y;
+        }      
+        
+    }
+    private Vector3 nextYVector;
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (_bounceHits < numberOfBounces)
+        {
+            nextYVector = new Vector3(transform.position.x, (transform.position.y + (_highestYCoordinate += nextBounceIncrease)), 0f);
+            transform.position = Vector3.Lerp(transform.position, nextYVector, Time.deltaTime * moveSpeed);
+        }
+        _bounceHits++;
+        Debug.Log(nextYVector.y);
+        Debug.Log(nextBounceIncrease);
+    }
+}
